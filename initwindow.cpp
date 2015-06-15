a0860251fe3fbcfb928cdf73f656899fd7664eb8
jelenajankov <jelena.jankov@gmail.com>
2015-06-16 00:01:50 +0200

    initwindow.cpp i game.cpp

--- initwindow.cpp
+++ initwindow.cpp
@@ -8,11 +8,9 @@ InitWindow::InitWindow(QWidget *parent) :
     ui(new Ui::InitWindow)
 {
     ui->setupUi(this);
- /*   ui->buttonWidget->setStyleSheet(QStringLiteral("border-image: url(./slike/2/init.jpg);"));
-    ui->pushButtonCreate->setStyleSheet(QStringLiteral("border-image: url();"));
-    ui->pushButtonLoad->setStyleSheet(QStringLiteral("border-image: url(pom.jpg);"));
-    ui->pushButtonExit->setStyleSheet(QStringLiteral("border-image: url();"));
-*/
+    ui->pushButtonLoad->setStyleSheet(QStringLiteral("border-image: url(./slike/Teme/2_load.jpg);"));
+    ui->inputIp->hide();
+    ui->labelIp->hide();
 }
 
 InitWindow::~InitWindow()
@@ -39,8 +37,12 @@ void InitWindow::on_pushButtonCreate_clicked()
 
 void InitWindow::on_pushButtonLoad_clicked()
 {
+    ui->pushButtonLoad->setStyleSheet(QStringLiteral("border-image: url(./slike/Teme/2_join.jpg);"));
+    ui->inputIp->show();
+    ui->labelIp->show();
+
     QString ipAddress = ui->inputIp->text();
-    int port = ui->inputPort->text().toInt();
+   /* int port = ui->inputPort->text().toInt();
 
     // osnovna provera - ne vrsi posao kad dodje do conn errora
     if( ipAddress == "" || port == 0 )
@@ -53,5 +55,5 @@ void InitWindow::on_pushButtonLoad_clicked()
     }
 
     cGame->show();
-    this->hide();
+    this->hide();*/
 }
